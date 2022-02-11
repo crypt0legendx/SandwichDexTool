9402315f27dd4dff47e4b4e6024830e51ea62243
real-fullstack-developer <95104528+real-fullstack-developer@users.noreply.github.com>
2022-02-11 08:37:15 -0800

    gainers and losers table responsive

--- Frontend/src/views/Currencies/Gainers-Losers/index.js
+++ Frontend/src/views/Currencies/Gainers-Losers/index.js
@@ -60,90 +60,99 @@ function GainersLosers() {
             <div className="row">
                 <div className="col-md-6">
                     <div className="sm-sub-title mt-5">Top Gainers</div>
-                    <table className="table mt-2">
-                        <thead>
-                            <tr>                                
-                                <th scope="col " className="text-left bg-light-gray theader-round-left">Position</th>
-                                <th scope="col " className="text-left bg-light-gray ">24h change</th>
-                                <th scope="col " className="text-right bg-light-gray ">volume</th>
-                                <th scope="col " className="text-right bg-light-gray theader-round-right">Trust Score</th>                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                filteredGainersForChain.slice(0,10).map((t,i)=>{
-                                    return (<tr key={i}>                                        
-                                        <td className="text-strong text-left ">                                                    
-                                            <Link to={ `/chart/${t.symbol}/${t.platform.token_address}`}>
-                                                <div className="d-flex align-items-center">
-                                                <img className="token-logo mr-2" src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${t.id}.png`} />
-                                                <div className="d-flex flex-column token-normal-info">
-                                                    <div>
-                                                        {t.name}
-                                                    </div>                                                        
-                                                    <div  className="small-price">
-                                                        ${t.price}
-                                                    </div>
-                                                </div>                                                                                                    
-                                                </div>
-                                            </Link>
-                                            </td>
-                                        <td className={parseFloat(t.percent_change_24h)>0?"text-success text-right":"text-danger text-right"}>
-                                            {Math.floor(t.percent_change_24h)}
-                                            %
-                                        </td>
-                                        <td className="text-strong text-right ">${Math.floor(t.volume_24h)}</td>
-                                        <td className="text-strong text-center"> 
-                                            N/A
-                                        </td>
-                                    </tr>);
-                                })
-                            }
-                        </tbody>
-                    </table>
+                    <div className="row">
+                        <div className="col-md-12"  style={{width:'100%', overflowX:'auto', textAlign:'center'}}>
+                            <table className="table mt-2">
+                                <thead>
+                                    <tr>                                
+                                        <th scope="col " className="text-left bg-light-gray theader-round-left">Position</th>
+                                        <th scope="col " className="text-left bg-light-gray ">24h change</th>
+                                        <th scope="col " className="text-right bg-light-gray ">volume</th>
+                                        <th scope="col " className="text-right bg-light-gray theader-round-right">Trust Score</th>                                
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        filteredGainersForChain.slice(0,10).map((t,i)=>{
+                                            return (<tr key={i}>                                        
+                                                <td className="text-strong text-left ">                                                    
+                                                    <Link to={ `/chart/${t.symbol}/${t.platform.token_address}`}>
+                                                        <div className="d-flex align-items-center">
+                                                        <img className="token-logo mr-2" src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${t.id}.png`} />
+                                                        <div className="d-flex flex-column token-normal-info">
+                                                            <div>
+                                                                {t.name}
+                                                            </div>                                                        
+                                                            <div  className="small-price">
+                                                                ${t.price}
+                                                            </div>
+                                                        </div>                                                                                                    
+                                                        </div>
+                                                    </Link>
+                                                    </td>
+                                                <td className={parseFloat(t.percent_change_24h)>0?"text-success text-right":"text-danger text-right"}>
+                                                    {Math.floor(t.percent_change_24h)}
+                                                    %
+                                                </td>
+                                                <td className="text-strong text-right ">${Math.floor(t.volume_24h)}</td>
+                                                <td className="text-strong text-center"> 
+                                                    N/A
+                                                </td>
+                                            </tr>);
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    
                 </div>
                 <div className="col-md-6">
                     <div className="sm-sub-title mt-5">Top Losers</div>
-                    <table className="table mt-2">
-                        <thead>
-                            <tr>                                
-                                <th scope="col " className="text-left bg-light-gray theader-round-left">Position</th>
-                                <th scope="col " className="text-left bg-light-gray ">24h change</th>
-                                <th scope="col " className="text-right bg-light-gray ">volume</th>
-                                <th scope="col " className="text-right bg-light-gray theader-round-right">Trust Score</th>                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                filteredLosersForChain.slice(0,10).map((t,i)=>{
-                                    return (<tr key={i}>                                        
-                                        <td className="text-strong text-left ">                                                    
-                                            <Link to={ `/chart/${t.symbol}/${t.platform.token_address}`}>
-                                                <div className="d-flex align-items-center">
-                                                <img className="token-logo mr-2" src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${t.id}.png`} />
-                                                <div className="d-flex flex-column token-normal-info">
-                                                    <div>
-                                                        {t.name}
-                                                    </div>                                                        
-                                                    <div className="small-price">
-                                                        ${t.price}
-                                                    </div>
-                                                </div>                                                                                                    
-                                                </div>
-                                            </Link>
-                                        </td>
-                                        <td className={parseFloat(t.percent_change_24h)>0?"text-success text-right":"text-danger text-right"}>
-                                            {Math.floor(t.percent_change_24h)}%
-                                        </td>
-                                        <td className="text-strong text-right ">${Math.floor(t.volume_24h)}</td>
-                                        <td className="text-strong text-center"> 
-                                            N/A
-                                        </td>
-                                    </tr>)
-                                })
-                            }
-                        </tbody>
-                    </table>
+                    <div className="row">
+                        <div className="col-md-12"  style={{width:'100%', overflowX:'auto', textAlign:'center'}}>
+                            <table className="table mt-2">
+                                <thead>
+                                    <tr>                                
+                                        <th scope="col " className="text-left bg-light-gray theader-round-left">Position</th>
+                                        <th scope="col " className="text-left bg-light-gray ">24h change</th>
+                                        <th scope="col " className="text-right bg-light-gray ">volume</th>
+                                        <th scope="col " className="text-right bg-light-gray theader-round-right">Trust Score</th>                                
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        filteredLosersForChain.slice(0,10).map((t,i)=>{
+                                            return (<tr key={i}>                                        
+                                                <td className="text-strong text-left ">                                                    
+                                                    <Link to={ `/chart/${t.symbol}/${t.platform.token_address}`}>
+                                                        <div className="d-flex align-items-center">
+                                                        <img className="token-logo mr-2" src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${t.id}.png`} />
+                                                        <div className="d-flex flex-column token-normal-info">
+                                                            <div>
+                                                                {t.name}
+                                                            </div>                                                        
+                                                            <div className="small-price">
+                                                                ${t.price}
+                                                            </div>
+                                                        </div>                                                                                                    
+                                                        </div>
+                                                    </Link>
+                                                </td>
+                                                <td className={parseFloat(t.percent_change_24h)>0?"text-success text-right":"text-danger text-right"}>
+                                                    {Math.floor(t.percent_change_24h)}%
+                                                </td>
+                                                <td className="text-strong text-right ">${Math.floor(t.volume_24h)}</td>
+                                                <td className="text-strong text-center"> 
+                                                    N/A
+                                                </td>
+                                            </tr>)
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
                                
