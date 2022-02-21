4d60928b5ed4a460510861faf74e85c135e9c252
real-fullstack-developer <95104528+real-fullstack-developer@users.noreply.github.com>
2022-02-21 10:13:27 -0800

    display trending datas in marquee

--- Frontend/src/components/TrendingMarquee/TrendingMarquee.js
+++ Frontend/src/components/TrendingMarquee/TrendingMarquee.js
@@ -1,30 +1,34 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import {useSelector} from "react-redux";
 import Marquee from "react-easy-marquee";
 
 import "./style.css";
 
 function TrendingMarquee() {
 
-    const [latestTrending, setLatestTrending ] = useState([
-        {name:'Bitcoin'},
-        {name:'Ethereum'},
-        {name:'Tether'},
-        {name:'BNB'},
-        {name:'BUSD'},
-        {name:'Solana'},
-        {name:'Doge'},
-        {name:'Polygon'},
-        {name:'Wrapped USDT'},
-        {name:'USDC'},
-    ]);
+    const trendings =  useSelector((state) => state.trendings.latest);
+    const [filteredTrendings, setFilteredTrendings] = useState([]);
+
+    useEffect(()=>{
+        
+        let ntrendings=[];
+        trendings.forEach(d => {
+            if(d.platform!=null&&(d.platform.slug == "ethereum"||d.platform.slug == "bnb" ||d.platform.slug == "matic")){
+              ntrendings.push(d);
+            }
+        });
+        setFilteredTrendings(ntrendings);
+    },[trendings]);
 
     return (
         <>
             <Marquee className="marquee-bar" duration={50000} background="#fafafa" height="50px" pauseOnHover={true} reverse={true}>
                 {
-                    latestTrending.map((t, i)=>{
+                    filteredTrendings.slice(0,10).map((t, i)=>{
                         return <button key={i} type="button " className="btn btn-primary px-md-2 coin-item mr-2">
-                                    &nbsp;{i+1}. {t.name}
+                                    &nbsp;{i+1}. 
+                                    <img className="coin-item-logo ml-1 mr-1" src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${t.id}.png`} />
+                                    {t.name}
                                 </button>
                     })
                 }                
